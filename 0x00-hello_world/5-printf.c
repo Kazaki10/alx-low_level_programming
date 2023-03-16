1d0e4099e5df7dd4661ebdf236caedf25f07a963
Kazaki10 <teakboy5@gmail.com>
2023-03-16 10:42:40 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - input
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
