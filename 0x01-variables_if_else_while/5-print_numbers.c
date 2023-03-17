064aa6832810e285a5785086e66c65d40dfcdd3b
Kazaki10 <teakboy5@gmail.com>
2023-03-17 13:48:56 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Entery
+ *
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int nm;
+
+	for (nm ='0'; nm <='9'; nm++)
+{
+		putchar(nm);
+}
+{
+		putchar('\n');
+}
+	return (0);
+}
