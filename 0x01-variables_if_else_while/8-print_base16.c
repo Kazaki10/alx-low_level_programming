9f03e48ae66e68c27b50605296cf8a589a92e370
Kazaki10 <teakboy5@gmail.com>
2023-03-17 14:17:03 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+* main - Entery
+*
+* Return: 0 (successful)
+*/
+int main(void)
+{
+	int nm, a;
+
+	for (nm = '0'; nm <= '9'; nm++)
+{
+	putchar(nm);
+}
+	for (a = 'a'; a <= 'f'; a++)
+{
+	putchar(a);
+}
+{
+	putchar('\n');
+}
+	return (0);
+}
