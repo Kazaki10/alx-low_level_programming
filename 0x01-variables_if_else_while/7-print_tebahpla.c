3734f621c649b71c031fa5bb0524d0ff32854974
Kazaki10 <teakboy5@gmail.com>
2023-03-17 14:08:00 -0700

    reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+* main - Entery
+*
+* Return: 0 (succesful)
+*/
+int main(void)
+{
+	int az;
+
+	for (az = 'z'; az >= 'a'; az--)
+{
+	putchar(az);
+}
+	putchar('\n');
+	return (0);
+}
