109da42b73a9e17a5d28607d78a158c93e21f689
Kazaki10 <teakboy5@gmail.com>
2023-03-21 16:34:19 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * return: 0 (success)
+ */
+void jack_bauer(void)
+{
+	int w, x, y, z;
+
+	for (w = 0; w <= 2; w++)
+	{
+		for (x = 0; x <= 9; x++)
+		{
+			for (y = 0; x <= 5; y++)
+			{
+				for (z = 0; z <= 9; z++)
+				{
+					if (w >= 2 && x >= 4)
+						break;
+					_putchar(w + '0');
+					_putchar(x + '0');
+					_putchar(':');
+					_putchar(y + '0');
+					_putchar(z + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
