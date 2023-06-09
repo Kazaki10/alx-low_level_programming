5dce684983b92f45556731fa4dad8a2b1871c7a1
Kazaki10 <teakboy5@gmail.com>
2023-06-09 14:56:41 +0100

    task-2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,19 +2,20 @@
 #include "main.h"
 
 /**
+ * main - prints all arguments it receives.
  *
- *
- *
- *
- *
+ * @argc: argumment count
+ * @argv: argument vektor
+ * Return: 0 always success
  */
 int main(int argc, char *argv[])
 {
 	int i;
-	
-	for(i = 0; i < argc; i++)
+
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
+	return (0);
 }
 
